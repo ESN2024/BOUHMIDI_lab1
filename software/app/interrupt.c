283f1c90cbc3338e2c252a3ae8cd13eb4ae02be8
RazanZai <144927751+RazanZai@users.noreply.github.com>
2024-01-16 11:07:25 +0100

    Mofication ISR

--- software/app/interrupt.c
+++ software/app/interrupt.c
@@ -13,6 +13,7 @@ static void arret_chenillard();
 static void irqhandler_bp_sw(void *context, alt_u32 id);
 
 volatile int edge_capture;
+int temps = 100000;			//valeur de vitesse par defaut au debut du programme
 
 static void init_interrupt_pio()
 {
@@ -20,29 +21,30 @@ static void init_interrupt_pio()
 	//autoriser l interruption de 2bp et 4 swicths
     IOWR_ALTERA_AVALON_PIO_IRQ_MASK(BP_SW_BASE,0x3F);
 
-    //Reset the edge capture register
+    //Reset le registre edge capture
     IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BP_SW_BASE,0x3F);
 
-    //Register the interrupt handler in the system  	
 	
 }
 
-static void chenillard(int temps)
+static void chenillard(int t)
 {
-    // int led_data;
-    // while (1)
-    // {
         int led_data = 0x01;
         IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE, 0x01);
-        usleep(temps);
+        usleep(t);
 
         while (led_data != 0x80)
         {
             led_data = led_data << 1;
             IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE, led_data);
-            usleep(temps);
+            usleep(t);
         }
-/*     } */
+		while(led_data != 0x01)
+		{
+			led_data= led_data>>1;
+			IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE,led_data);
+			usleep(t);	
+		}
 }
 
 static void arret_chenillard()
@@ -52,40 +54,35 @@ static void arret_chenillard()
 
 static void irqhandler_bp_sw (void * context, alt_u32 id)
 {
-	alt_u32 interrupt = IORD_ALTERA_AVALON_PIO_IRQ_MASK(BP_SW_BASE);
 	alt_u32 val = IORD_ALTERA_AVALON_PIO_EDGE_CAP(BP_SW_BASE);
-    volatile int* edge_capture_ptr = (volatile int*) context;
-    *edge_capture_ptr = IORD_ALTERA_AVALON_PIO_EDGE_CAP(BP_SW_IRQ);
 	switch (val) 
 	{
-		case 0x01:
-			//lancement chenillard
-			chenillard(100000);
-		break;
-		case 0x02:
-			//arret
-			arret_chenillard();
-		break;
 		case 0x04:
-			//lancement chenillard Vitesse +
-			chenillard(80000);
+			//Vitesse +
+			temps = 80000;
 		break;
 		case 0x08:
-			//lancement chenillard Vitesse ++
-			chenillard(60000);
+			//Vitesse ++
+			temps = 60000;
 		break;
 		case 0x10:
-			//lancement chenillard Vitesse +++
-			chenillard(40000);
+			//Vitesse +++
+			temps = 40000;
 		break;
 		case 0x20:
-			//lancement chenillard Vitesse ++++
-			chenillard(20000);
+			//Vitesse ++++
+			temps = 20000;
 		break;
-		default :
-			IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE, 0x0F);
-		break;
-	}		
+	}
+	
+	if (val == 0x01)
+	{
+		chenillard(temps);
+		//temps = 100000;         //decommenter si l'on souhaite toujours avoir une vitesse par défaut 
+	}
+	else if (val == 0x02)
+		arret_chenillard();
+		
     IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BP_SW_BASE,0x3F);
 
 }
